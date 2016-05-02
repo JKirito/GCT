210ff68456ce23263d9eb1f3381f5036cec93d78
JGodoy <j.godoy277@gmail.com>
2016-05-01 22:57:05 -0300

    test

--- src/test/EjemploTest.java
+++ src/test/EjemploTest.java
@@ -26,8 +26,97 @@ public class EjemploTest
 	public void m0_3()
 	{
 		Ejemplo var = new Ejemplo();
-		Integer ret = var.m0(5, 6);
+		Integer ret = var.m0(6, 1);
 		assertEquals(new Integer(2), ret);
+	}
+
+	@Test
+	public void m0_4()
+	{
+		Ejemplo var = new Ejemplo();
+		Integer ret = var.m0(4, 1);
+		assertEquals(new Integer(2), ret);
+	}
+
+	@Test
+	public void m2_1()
+	{
+		Ejemplo var = new Ejemplo();
+		var.m2(0);
+	}
+
+	@Test
+	public void m2_2()
+	{
+		Ejemplo var = new Ejemplo();
+		var.m2(1);
+	}
+
+	@Test
+	public void m2_3()
+	{
+		Ejemplo var = new Ejemplo();
+		var.m2(2);
+	}
+
+	@Test
+	public void m2_4()
+	{
+		Ejemplo var = new Ejemplo();
+		var.m2(3);
+	}
+
+	@Test
+	public void m2_5()
+	{
+		Ejemplo var = new Ejemplo();
+		var.m2(5);
+	}
+
+	@Test
+	public void getMax_1()
+	{
+		Ejemplo var = new Ejemplo();
+		Integer ret = var.getMax(0, 0, 0);
+		assertEquals(new Integer(0), ret);
+	}
+
+	@Test
+	public void getMax_2()
+	{
+		Ejemplo var = new Ejemplo();
+		Integer ret = var.getMax(-2, -1, 0);
+		assertEquals(new Integer(0), ret);
+	}
+
+	@Test
+	public void getMax_3()
+	{
+		Ejemplo var = new Ejemplo();
+		Integer ret = var.getMax(-1, 0, -2);
+		assertEquals(new Integer(0), ret);
+	}
+
+	@Test
+	public void getMax_4()
+	{
+		Ejemplo var = new Ejemplo();
+		Integer ret = var.getMax(0, -1, -1);
+		assertEquals(new Integer(0), ret);
+	}
+
+	@Test
+	public void m1_1()
+	{
+		Ejemplo var = new Ejemplo();
+		var.m1(0, 0);
+	}
+
+	@Test
+	public void m1_2()
+	{
+		Ejemplo var = new Ejemplo();
+		var.m1(3, 0);
 	}
 
 }
